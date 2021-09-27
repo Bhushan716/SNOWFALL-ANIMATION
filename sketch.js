958d45a022ad66b324756110a159e062d51f5231
Bhushan716 <80574088+Bhushan716@users.noreply.github.com>
2021-09-27 02:16:27 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+function preload(){
+  bgimg = loadImage("snow3.jpg");
+
+}
+
+
+
+
+
+
+
+function setup() {
+
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(bgimg);  
+  drawSprites();
+}
